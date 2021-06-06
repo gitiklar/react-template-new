4db9ad0267c9d98ed3962118ddc83319cc8d603c
gitiklar <65737909+gitiklar@users.noreply.github.com>
2021-06-06 18:44:40 +0300

    update

--- without-webpack/src/redux/middlewares.js
+++
@@ -1,7 +0,0 @@
-
-export const middleware1 = ({getState}) => next => action => {
-    const nextAction = next(action);
-    if(action.type === 'MIDDLEWARE1') return nextAction;
-
-    return nextAction;
-};
